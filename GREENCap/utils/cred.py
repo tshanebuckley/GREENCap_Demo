67e56b53c7bf4514bcc344d93d80dab8e2eee495
Josh Ackerman <Joshackerman417@gmail.com>
2021-12-01 09:49:29 -0500

    added validations to Cred class

--- GREENCap/utils/cred.py
+++ GREENCap/utils/cred.py
@@ -9,4 +9,27 @@ class Cred(pydantic.BaseModel):
     token: str
     local: boolean
     cli: boolean
-    cred: Optional[Cred] 
+    cred: Optional[Cred]
+
+    if cred:
+        name = cred.name
+        url = cred.url
+        local = cred.local
+        cli = cred.cli
+
+
+    @pydantic.root_validator(pre=True)
+    @classmethod
+    def check_connection(cls, value):
+        try:
+            redcap.Project(value["url"], value["token"])
+        except:
+            raise REDCapConnectError(name=values["name"],
+            message="Unable to connect to REDCap project {name}.".format(name=values["name"]))
+        return value
+
+    @pydantic.root_validator(pre=True)
+    @classmethod
+    def check_name(cls, value):
+        if value["name"] == None and value["cli"] == False:
+            raise InvalidNameError(message="Name must be provided.")
