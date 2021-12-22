120d31d6c16dc219cddc6c05d2e1ffc68944df75
Virmani12 <virmania12@gmail.com>
2021-12-22 16:25:22 -0500

    Added basic DTale option to gui

--- GREENCap/gui/RShiny/redcap_webapp.R
+++ GREENCap/gui/RShiny/redcap_webapp.R
@@ -82,41 +82,95 @@ ui <- fluidPage(theme = shinytheme('cerulean'),
         tableOutput('table'),
         
         # Download data table as csv
-        downloadButton('downloadData', 'Download')
+        downloadButton('downloadData', 'Download'),
+        
+        downloadButton("fakeDownload", label = "Download and Open D-Tale")
+        
       )
     ),
     
     # Project adding tab
     tabPanel(
-      "Add a Project",
       
-      sidebarPanel(
-        
-        # User requirements for adding a project to REDCap database
-        tags$h3('Project Details:'),
-        
-        helpText('Add a project to REDCap. Provide project name and API information.'),
-        
-        textInput('new_proj', 'Project Name:', ''),
-        
-        textInput('new_URL', 'Project URL:', ''),
-        
-        textInput('new_token', 'Project Token:', ''),
-        
-        # Execute adding a project
-        actionButton('add_Proj', 'Add Project'),
-        
-        verbatimTextOutput('add_results')
-        
-      ),
+      title = "Add, Remove, Update a Project",
       
-      mainPanel(
-        
-        # Display success/error in adding project
-        h1('Status:'),
-        
-        verbatimTextOutput('status')
+      h1('Status:'),
+      
+      verbatimTextOutput('status'),
+      
+      hr(),
+      
+      fluidRow(
+        column(3,
+               # User requirements for adding a project to REDCap database
+               #tags$h3('Project Details:'),
+               
+               helpText('Add a project on REDCap. Please provide the project name, url, and token.'),
+               
+               textInput('new_proj', 'Project Name:', ''),
+               
+               textInput('new_URL', 'Project URL:', ''),
+               
+               textInput('new_token', 'Project Token:', ''),
+               
+               # Execute adding a project
+               actionButton('add_Proj', 'Add Project'),
+               
+               verbatimTextOutput('add_results')
+               
+        ),
+        column(4, offset = 1,
+               helpText('Remove a project on REDCap. Please provide the project name. '),
+               
+               textInput('rem_proj', 'Project Name:', ''),
+               
+               actionButton('remove', 'Remove Project'),
+               
+               verbatimTextOutput('rem_results')
+        ),
+        column(4,
+               helpText('Update a project on REDCap. Please provide the updated project name, url, or token.'),
+               
+               textInput('upd_proj', 'New Project Name:', ''),
+               
+               textInput('upd_URL', 'New Project URL:', ''),
+               
+               textInput('upd_token', 'New Token URL', ''),
+               
+               actionButton('update', 'Update Project'),
+               
+               verbatimTextOutput('upd_results')
+        )
       )
+      # "Add a Project",
+      # 
+      # sidebarPanel(
+      #   
+      #   # User requirements for adding a project to REDCap database
+      #   tags$h3('Project Details:'),
+      #   
+      #   helpText('Add a project on REDCap. Please provide the project name, url, and token.'),
+      #   
+      #   textInput('new_proj', 'Project Name:', ''),
+      #   
+      #   textInput('new_URL', 'Project URL:', ''),
+      #   
+      #   textInput('new_token', 'Project Token:', ''),
+      #   
+      #   # Execute adding a project
+      #   actionButton('add_Proj', 'Add Project'),
+      #   
+      #   verbatimTextOutput('add_results')
+      #   
+      # ),
+      # 
+      # mainPanel(
+      #   
+      #   # Display success/error in adding project
+      #   h1('Status:'),
+      #   
+      #   verbatimTextOutput('status')
+      # )
     ),
     
     # Adding a pipe tab
@@ -147,6 +201,8 @@ server <- function(input, output) {
   
   # Reactive string of parameters for url fetch
   querystring <- reactiveVal()
+  
+  csv_file <- reactiveVal()
   
   # Parameter specific urls for getting dropdowns
   formsurl <- 'http://127.0.0.1:8000/forms/'
@@ -290,6 +346,7 @@ server <- function(input, output) {
     
     # create dataframe for output
     df <- data.frame(do.call('cbind', json_file))
+    csv_file(df)
     
   
     #Output data table to UI
@@ -307,8 +364,11 @@ server <- function(input, output) {
       },
       content = function(file){
         write.csv(df, file)
+        print(file)
       }
     )
+    
+    
     
   })
   
@@ -317,13 +377,41 @@ server <- function(input, output) {
     paste0(baseurl,projecturl(), '/?', querystring())
   })
   
+  
+  
+  #observeEvent(input$openDtale, {
+    
+  output$fakeDownload <- downloadHandler(
+    fname <- stringr::str_replace_all(stringr::str_replace(Sys.time(), " ", "_"), ":", "_"),
+    filename = function(){
+      paste("data-", fname, ".csv", sep="")
+    },
+    content = function(file){
+      if (!dir.exists('~/.greencap/dtale')){
+        dir.create("~/.greencap/dtale")
+      }
+      write.csv(csv_file(), paste0('~/.greencap/dtale/data-',fname, '.csv'))
+      
+      system(paste0("python3 open_dtale.py ", paste0('~/.greencap/dtale/data-',fname, '.csv')))
+    }
+  )
+    #)
+    
+    #filename <- paste0("data-", Sys.time(), ".csv")
+    
+    
+    #csv_file(paste0('~/.greencap/dtale/', filename))
+    #write.csv(df, paste0('~/.greencap/dtale/', filename))
+    
+    
+  
+  
   ###############################################
   # Add a Project
   
   # function to add a project to either the local system or lab database
   # Currently only handles local functionality
-  # TODO: fix reactive handling of error cases (redundant project, empty fields
-  # general errors)
+  
   create_project <- function(name, url, token, local=TRUE){
     
     tryCatch({
@@ -372,6 +460,97 @@ server <- function(input, output) {
     
   }
   
+  delete_project <- function(name, local=TRUE){
+    
+    tryCatch({
+      
+      if(local){
+        
+        # access path with projects
+        greencap_user_cfg <- path.expand('~/.greencap/projects')
+        
+        # retrieve all files in 'projects' folder
+        local_files <- list.files(greencap_user_cfg)
+        
+        # add extension to project name for json creation
+        name_ext <- paste0(name, ".json")
+        
+        # if file is already located locally, return FALSE and inform user
+        if (name_ext %in% local_files){
+          
+          file_to_remove <- paste0('~/.greencap/projects/',name_ext)
+          file.remove(file_to_remove)
+          
+          return(TRUE)
+        
+        } else{
+          
+          output$rem_results <- renderText({
+            paste0("Project not contained locally!")
+            
+          })
+          return(FALSE)
+        }
+      }
+    }, error = function(e){
+      
+      # error message if all else fails and return FALSE
+      output$rem_results <- renderText({
+        paste0("Error in project creation.")
+      })
+      return(FALSE)
+      
+    })
+  }
+  
+  update_project <- function(name, url, token, local=TRUE){
+    
+    tryCatch({
+      
+      if(local){
+        
+        # access path with projects
+        greencap_user_cfg <- path.expand('~/.greencap/projects')
+        
+        # retrieve all files in 'projects' folder
+        local_files <- list.files(greencap_user_cfg)
+        
+        # add extension to project name for json creation
+        name_ext <- paste0(name, ".json")
+        
+        # if file is already located locally, return FALSE and inform user
+        if (name_ext %in% local_files){
+          
+          # create 2-D vector of url and token
+          redcap_cred_list <- list(url,token)
+          names(redcap_cred_list) <- c("url", "token")
+          
+          # convert vector to json file and write to 'projects' folder
+          json_proj <- toJSON(redcap_cred_list)
+          write(json_proj, paste0('~/.greencap/projects/',name,".json"))
+          
+        } else{
+          
+          output$upd_results <- renderText({
+            paste0("Project not contained locally!")
+            
+          })
+          return(FALSE)
+        }
+      }
+    }, error = function(e){
+      
+      # error message if all else fails and return FALSE
+      output$upd_results <- renderText({
+        paste0("Error in project creation.")
+      })
+      return(FALSE)
+      
+    })
+    
+    return(TRUE)
+  }
+  
   
   # if the user selects "add project"
   observeEvent(input$add_Proj, {
@@ -410,6 +589,71 @@ server <- function(input, output) {
       }
     }
       
+  })
+  
+  
+  observeEvent(input$remove, {
+    
+    if(input$rem_proj == ''){
+      output$rem_results <- renderText({
+        paste('Please provide all required information.')
+      })
+    }
+    
+    else{
+      
+      results <- delete_project(input$rem_proj, local = TRUE)
+      
+      if(results){
+        
+        output$status <- renderText({
+          paste0('Project Removed Successfully: ', input$rem_proj)
+          
+        })
+        output$rem_results <- renderText({
+          paste('')
+        })
+      }
+    }
+    
+  })
+  
+  observeEvent(input$update, {
+    
+    # if fields are empty, return FALSE and let the user know 
+    # (shouldn't be reactive)
+    if(input$upd_proj == '' | input$upd_URL == '' | input$upd_token == ''){
+      output$upd_results <- renderText({
+        paste('Please provide all required information.')
+      })
+      
+    }
+    
+    else{
+      
+      # attempt to create project with inputted fields, returns boolean
+      results <- update_project(input$upd_proj, input$upd_URL, input$upd_token, local = TRUE)
+      
+      # if true, project was added successfully
+      if(results){
+        
+        output$status <- renderText({
+          paste0("Project Updated Successfully: ", input$upd_proj)
+          
+        })
+        output$upd_results <- renderText({
+          paste('')
+        })
+      }
+      
+      # If false, project was not added successfully
+      else{
+        output$status <- renderText({
+          paste0("Project could not be updated: ", input$upd_proj)
+        })
+      }
+    }
+    
   })
   
 }
